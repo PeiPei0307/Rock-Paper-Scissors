27eac3865080896484a04fe52473e4ab1b7e073a
PeiPei0307 <peggyuabc@gmail.com>
2022-01-13 16:16:28 +0800

    111

--- test.py
+++ test.py
@@ -1,12 +1,54 @@
-import os, time, socket, threading
+import argparse, socket, threading, time
 
-from server import Server
-from client import Client
-a = 999
+Test test
 
-def function3():
-    global a
-    a += 1
-    print(a)
+def server_thread(sc, count):
+    while True:
+        data = sc.recv(1024)
+        print('  Client at ',  sc.getpeername(), ' message:', repr(data) ,)
+        message = 'Your data was {} bytes long'.format(len(data))
+        sc.sendall(message.encode('utf-8'))
+        if data == b"|exit|":
+            break
+    sc.close()
+    print('  Reply sent, socket closed')
 
-if __name__ == '__main__': 
+def server(interface, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((interface, port))
+    sock.listen(1)
+    while True:
+        print('Listening at', sock.getsockname())
+        print('Waiting to accept a new connection')
+        sc, sockname = sock.accept()
+        print('We have accepted a connection from', sockname)
+        print('  Socket name:', sc.getsockname())
+        print('  Socket peer:', sc.getpeername())
+        sc = threading.Thread(target = server_thread, args = (sc, 6))
+        sc.start()
+
+def client(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    print('Client has been assigned socket name', sock.getsockname())
+    while True:
+        message = input(">>>")
+        sock.sendall(message.encode('utf-8'))
+        reply = sock.recv(1024)
+        print('The server said', repr(reply))
+        if message == "|exit|":
+            break
+    sock.close()
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive over TCP')
+    parser.add_argument('role', choices=choices, help='which role to play')
+    parser.add_argument('host', help='interface the server listens at;'
+                        ' host the client sends to')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
